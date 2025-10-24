b8268dc473e20472c335b946900ac7599b4edcb8
hquangthinh13 <hquangthinh13@gmail.com>
2025-10-24 21:38:24 +0700

    Update formatDate.js

--- src/lib/formatDate.js
+++ src/lib/formatDate.js
@@ -2,11 +2,14 @@ export const formatDate = (createdAt) => {
   const now = new Date();
   const posted = new Date(createdAt);
   const diffMs = now - posted;
+  const diffSeconds = Math.floor(diffMs / 1000);
   const diffMinutes = Math.floor(diffMs / (1000 * 60));
   const diffHours = Math.floor(diffMs / (1000 * 60 * 60));
   const diffDays = Math.floor(diffMs / (1000 * 60 * 60 * 24));
 
-  if (diffMinutes < 60) {
+  if (diffSeconds < 30) {
+    return "just now";
+  } else if (diffMinutes < 60) {
     return `${diffMinutes} minute${diffMinutes !== 1 ? "s" : ""} ago`;
   } else if (diffHours < 24) {
     return `${diffHours} hour${diffHours !== 1 ? "s" : ""} ago`;
